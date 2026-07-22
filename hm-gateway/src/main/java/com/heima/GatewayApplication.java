3b1cab71b6d75ded7b61b791321a0db17abaa083
AechWhyL <2933964395@qq.com>
2026-07-22 17:47:42 +0800

    init

---
+++ hm-gateway/src/main/java/com/heima/GatewayApplication.java
@@ -0,0 +1,11 @@
+package com.heima;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
